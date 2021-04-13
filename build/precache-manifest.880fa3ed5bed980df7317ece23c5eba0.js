865789d493a900bf0fbbfb46cc1a742caf6704cf
paul <paul86677889@gmail.com>
2021-04-13 09:21:00 +0800

    prod

---
+++ build/precache-manifest.880fa3ed5bed980df7317ece23c5eba0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4152cae98df519aece",
+    "url": "./app/app.js"
+  },
+  {
+    "revision": "7f45a8dd13bf8b6a01e1a12dd9a81dce",
+    "url": "./custom.html"
+  },
+  {
+    "revision": "eb2daf5c438e3bd33214",
+    "url": "./custom/custom.js"
+  },
+  {
+    "revision": "4b4c4a971461a434fa3911c2f0d0f488",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc3018e892796d3886835c621096c668",
+    "url": "./map.html"
+  },
+  {
+    "revision": "90f19389a45d2d17b2a0",
+    "url": "./map/map.js"
+  },
+  {
+    "revision": "8bd3e31fc8975257f4fe",
+    "url": "./runtime-app/runtime-app.js"
+  },
+  {
+    "revision": "abdbf052682751608b5a",
+    "url": "./runtime-custom/runtime-custom.js"
+  },
+  {
+    "revision": "862bf264b139f761d0aa",
+    "url": "./runtime-map/runtime-map.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/app.desktop.s.css"
+  },
+  {
+    "revision": "c2083727da73721707354a7eeece0bc7",
+    "url": "./static/css/app.mobile.s.css"
+  },
+  {
+    "revision": "6f4152cae98df519aece",
+    "url": "./static/css/app.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/app.tablet.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/app.tabletLandscape.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/app.tabletPortrait.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/map.desktop.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/map.mobile.s.css"
+  },
+  {
+    "revision": "90f19389a45d2d17b2a0",
+    "url": "./static/css/map.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/map.tablet.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/map.tabletLandscape.s.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/css/map.tabletPortrait.s.css"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "./static/media/logo.svg"
+  },
+  {
+    "revision": "b0f471c2600c143bfc95",
+    "url": "./vendors~app/vendors~app.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./vendors~app/vendors~app.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5d7f4082d3b43406f6",
+    "url": "./vendors~map/vendors~map.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./vendors~map/vendors~map.js.LICENSE.txt"
+  }
+]);
